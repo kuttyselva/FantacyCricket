2d251ea78739ec5b7296f24e18224f14e083f34e
kuttyselva <kuttyselva57@gmail.com>
2019-06-11 01:03:21 +0530

    front work done

--- fantacy/src/components/Display.js
+++ fantacy/src/components/Display.js
@@ -10,7 +10,13 @@ class Display extends Component {
       bow:[],
       wc:[],
       all:[],
-      point:100
+      point:100,
+      btc:0,
+      bwc:0,
+      wcc:0,
+      arc:0,
+      btn:'btn btn-dark'
+      
     }
     componentDidMount(){
       this.getbat();
@@ -44,74 +50,92 @@ class Display extends Component {
       
     }
     renderbat=()=>(
-      this.state.bat.data?this.state.bat.data.map((data,ind)=>(
+      this.state.bat.data?
+      (this.state.btc<5 && this.props.play.point>0)?
+      this.state.bat.data.map((data,ind)=>(
         <div className="card col-2" style={{width:'15rem',margin:'1rem'}}>
         <img className="card-img-top img-thumbnail" src={data.img} alt="Card image cap"/>
         <div className="card-body text-justify">
           <h5 className="card-title">{data.name}</h5>
           <p className="card-text">{data.pos} - {data.team}</p>
-          <a href="#" className="btn btn-primary" onClick={()=>{ 
+          <a href="#" className={this.state.btn} onClick={()=>{ 
             this.props.select(this.props.play.point-data.point)
             this.props.players(data)
+            this.setState({btc:this.state.btc+1})
             
+
             }}>Points {data.point}</a>
         </div>
       </div>     
-      )):null
+      )):alert("Batsmen Limit is 4 :p refresh to Choose from first")
+      :null
     )
     renderbow=()=>(
-      this.state.bow.data?this.state.bow.data.map((data,ind)=>(
+      this.state.bow.data?
+      (this.state.bwc<5 &&this.props.play.point>0)?
+      this.state.bow.data.map((data,ind)=>(
         <div className="card col-2" style={{width:'15rem',margin:'1rem'}}>
         <img className="card-img-top img-thumbnail" src={data.img} alt="Card image cap"/>
         <div className="card-body text-justify">
           <h5 className="card-title">{data.name}</h5>
           <p className="card-text">{data.pos} - {data.team}</p>
-          <a href="#" className="btn btn-primary" onClick={()=>{ 
+          <a href="#" className={this.state.btn} onClick={()=>{ 
             this.props.select(this.props.play.point-data.point)
             this.props.players(data)
-            
+            this.setState({bwc:this.state.bwc+1})
+           
             }}>Points {data.point}</a>
         </div>
       </div>         
-      )):null
+      )):alert("Bowlers Limit is 4 :p refresh to Choose from first")
+      :null
     )
   
     renderall=()=>(
-      this.state.all.data?this.state.all.data.map((data,ind)=>(
+      this.state.all.data?
+      (this.state.arc<2 && this.props.play.point>0)?
+      this.state.all.data.map((data,ind)=>(
         <div className="card col-2" style={{width:'15rem',margin:'1rem'}}>
         <img className="card-img-top img-thumbnail" src={data.img} alt="Card image cap"/>
         <div className="card-body text-justify">
           <h5 className="card-title">{data.name}</h5>
           <p className="card-text">{data.pos} - {data.team}</p>
-          <a href="#" className="btn btn-primary" onClick={()=>{ 
+          <a href="#" className={this.state.btn} onClick={()=>{ 
             this.props.select(this.props.play.point-data.point)
             this.props.players(data)
-            
+            this.setState({arc:this.state.arc+1})
+           
             }}>Points {data.point}</a>
         </div>
       </div>         
-      )):null
+      )):alert("All Rounder Limit is 1 :p refresh to Choose from first")
+      :null
     )
     renderwc=()=>(
-      this.state.wc.data?this.state.wc.data.map((data,ind)=>(
+      this.state.wc.data?
+      (this.state.wcc<2 && this.props.play.point>0 )?
+      this.state.wc.data.map((data,ind)=>(
         <div className="card col-2" style={{width:'15rem',margin:'1rem'}}>
         <img className="card-img-top img-thumbnail" src={data.img} alt="Card image cap"/>
         <div className="card-body text-justify">
           <h5 className="card-title">{data.name}</h5>
           <p className="card-text">{data.pos} - {data.team}</p>
-          <a href="#" className="btn btn-primary" onClick={()=>{ 
+          <a href="#" className={this.state.btn} onClick={()=>{ 
             this.props.select(this.props.play.point-data.point)
             this.props.players(data)
+            this.setState({wcc:this.state.wcc+1})
             
             }}>Points {data.point}</a>
         </div>
       </div>           
-      )):null
+      )):alert("Wicket Keeper Limit is 1 :p refresh to Choose from first")
+      :null
     )
     render(){
     
     return (
       <div classNameName="container"><br/>
+      {console.log(this.state)}
         <button className="btn btn-dark"><h1 className="display-3">Batsmen</h1></button><br/>
         <div className="row">
        {this.renderbat()}
